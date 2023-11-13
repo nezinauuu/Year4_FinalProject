de9cdcbfd9d583e2839c2d46191e1c301d8bfc3a
nezinauuu <B00133471@mytudublin.ie>
2023-11-13 14:19:15 +0000

    Fixed image resizing

--- year4project/app/page.tsx
+++ year4project/app/page.tsx
@@ -3,7 +3,9 @@ import backgroundImage from "@/images/background-top.svg"; // import the SVG fil
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import { UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
+import luckyImage  from "@/images/lucky.jpg";
 export default function Home() {
   const [animalName, setAnimalName] = useState();
   const [litterSize, setLitterSize] = useState();
@@ -29,6 +31,10 @@ export default function Home() {
   return (
     <main className="flex flex-col p-12 bg-[url('../images/background-top.svg')] bg-cover min-h-screen">
       <title>Pets</title>
+      <div className="fixed top-10 right-10 flex">
+        <UserButton />
+      </div>
+
       <div className="flex justify-center py-4">
         <h1 className=" backdrop-blur-md bg-white/10 drop-shadow-lg font-extrabold sm:text-[3rem] py-8 px-4 max-w-4xl text-white">
           Welcome to Pets App
@@ -36,8 +42,14 @@ export default function Home() {
         </h1>
       </div>
 
-      <div className="flex justify-center flex-row py-9  gap-32  m-w-64">
-        <div className="flex bg-[url('../images/lucky.jpg')] bg-cover box-border w-96 h-96 p-4 transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 border-4 border-gray-900"></div>
+      <div className="flex justify-center flex-row py-9 gap-16 m-w-64">
+      
+          <img
+            src={luckyImage.src}
+            alt="Lucky"
+            className="object-cover w-full h-full rounded-xl max-w-md min-w-[200px] border-red-300 border-4"
+          />
+   
         <div className="flex flex-col justify-center max-w-2xl gap-5">
           <div className="backdrop-blur-md bg-white/10 border-4 transition ease-in-out hover:-translate-y-0 hover:scale-95 duration-100">
             <h1 className="text-4xl font-bold text-white m-3">Your Pets</h1>
