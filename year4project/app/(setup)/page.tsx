4ef881193123745774bfb4ed03e10fb456219e17
nezinauuu <B00133471@mytudublin.ie>
2024-01-31 20:12:28 +0000

    Fixed modal
    agony.

--- year4project/app/(setup)/page.tsx
+++ year4project/app/(setup)/page.tsx
@@ -1,4 +1,3 @@
-
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -11,96 +10,89 @@ import { User } from "lucide-react";
 import luckyImage from "@/images/lucky.jpg";
 import Bulldog from "@/images/Bulldog.jpg";
 import { ScrollToTop } from "@/components/ui/ScrollToTop";
-
+import { SignIn, SignUp } from "@clerk/nextjs";
+import { AddPetForm } from "@/components/ui/modals/add-pet";
 const Home = async () => {
+  const profile = await initialProfile();
+  const pet = await db.pet.findFirst({
+    where: {
+      animalKeepers: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
+  if (pet) {
+    return redirect(`/pets/${pet.id}`);
+  }
 
-    const profile = await initialProfile();
-    const pet = await db.pet.findFirst({
-        where: {
-            animalKeepers: {
-                some: {
-                    id: profile.id,
-                },
-            },
-        }
-    })
-
-    
-
-    if (pet) {
-        return redirect(`/pets/${pet.id}`);
-    }
-
-    return (
-      <main>
-        <title>Pets</title>
-        <div className="flex flex-col p-12 bg-[url('../images/background-top.svg')] bg-cover min-h-screen">
-          <div className="fixed top-10 right-10 flex hover:-translate-y-0 hover:scale-90 duration-100"></div>
-          <div className="fixed top-10 right-10 flex hover:-translate-y-0 hover:scale-90 duration-100">
-            <UserButton />
-          </div>
-
-          <div className="flex justify-center py-4">
-            <h1 className=" backdrop-blur-md bg-white/10 drop-shadow-lg font-extrabold sm:text-[3rem] py-8 px-4 max-w-4xl text-white">
-              Welcome to Pets App
-            </h1>
-          </div>
-          {/* <AddPet /> */}
-
-          <div className="flex justify-center flex-row py-9 gap-16 m-w-64">
-            <img
-              src={luckyImage.src}
-              alt="Lucky"
-              className="object-cover w-full h-full rounded-xl max-w-md min-w-[200px] border-red-300 border-4"
-            />
-
-            <div className="flex flex-col justify-center max-w-2xl gap-5">
-              <div className="backdrop-blur-md bg-white/10 border-4 transition ease-in-out hover:-translate-y-0 hover:scale-95 duration-100">
-                <h1 className="text-4xl font-bold text-white m-3">Your Pets</h1>
-                <p className="text-white m-3">
-                  Review your pets and share your experience with others.
-                </p>
-              </div>
-
-              <ScrollToTop />
-              <h1 className="text-4xl font-bold text-white m-3">
-                Looking for a new animal?
-              </h1>
-              <p className="text-white m-3">
-                Check out important information about the animal you want to
-                adopt.
-              </p>
-            </div>
+  return (
+    <main>
+      <title>Pets</title>
+      <div className="flex flex-col p-12 bg-[url('../images/background-top.svg')] bg-cover min-h-screen">
+        <div className="fixed top-10 right-10 flex hover:-translate-y-0 hover:scale-90 duration-100"></div>
+        <div className="fixed top-10 right-10 flex hover:-translate-y-0 hover:scale-90 duration-100">
+          <UserButton />
+          <div className="fixed top-10 right-10 flex gap-4 hover:-translate-y-0 hover:scale-90 duration-100">
+            <SignIn />
+            <SignUp />
           </div>
         </div>
 
-        <div
-          id="new-animal-section"
-          className="flex flex-col p-12 bg-[url('../images/background-bot.svg')] bg-cover min-h-screen "
-        >
-          <h1 className="text-4xl font-bold text-white m-3 flex justify-end">
-            Looking for a new animal?
+        <div className="flex justify-center py-4">
+          <h1 className=" backdrop-blur-md bg-white/10 drop-shadow-lg font-extrabold sm:text-[3rem] py-8 px-4 max-w-4xl text-white">
+            Welcome to Pets App
           </h1>
-          <p className="text-white m-3 flex justify-end">
-            Check out important information about the animal you want to adopt.
-          </p>
+        </div>
+        {/* <AddPet /> */}
 
-          <div className="flex justify-center flex-col gap-32">
-            <div className="backdrop-blur-md bg-white/10 border-4 transition ease-in-out hover:-translate-y-0 hover:scale-95 duration-100"></div>
+        <div className="flex justify-center flex-row py-9 gap-16 m-w-64">
+          <img
+            src={luckyImage.src}
+            alt="Lucky"
+            className="object-cover w-full h-full rounded-xl max-w-md min-w-[200px] border-red-300 border-4"
+          />
 
-            <div>
-              <img
-                src={Bulldog.src}
-                className="object-cover w-full h-full rounded-xl max-w-md min-w-[200px] border-red-300 border-4 "
-              ></img>
-            </div>
+          <div className="flex flex-col justify-center max-w-2xl gap-5">
+            <AddPetForm />
+
+            <ScrollToTop />
+            <h1 className="text-4xl font-bold text-white m-3">
+              Looking for a new animal?
+            </h1>
+            <p className="text-white m-3">
+              Check out important information about the animal you want to
+              adopt.
+            </p>
           </div>
         </div>
-      </main>
-    );
+      </div>
 
-    
+      <div
+        id="new-animal-section"
+        className="flex flex-col p-12 bg-[url('../images/background-bot.svg')] bg-cover min-h-screen "
+      >
+        <h1 className="text-4xl font-bold text-white m-3 flex justify-end">
+          Looking for a new animal?
+        </h1>
+        <p className="text-white m-3 flex justify-end">
+          Check out important information about the animal you want to adopt.
+        </p>
 
-}
-export default Home
+        <div className="flex justify-center flex-col gap-32">
+          <div className="backdrop-blur-md bg-white/10 border-4 transition ease-in-out hover:-translate-y-0 hover:scale-95 duration-100"></div>
+
+          <div>
+            <img
+              src={Bulldog.src}
+              className="object-cover w-full h-full rounded-xl max-w-md min-w-[200px] border-red-300 border-4 "
+            ></img>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Home;
--- year4project/components/ui/modals/add-pet.tsx
+++ year4project/components/ui/modals/add-pet.tsx
@@ -1,26 +1,157 @@
-// AddPetForm.js
-import { useState } from "react";
+"use client";
+
 import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
+import { useEffect, useState } from "react";
+import { Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-type AddPetFormProps = {
-  onSubmit: (data: any) => void;
-};
+const schema = Joi.object({
+  name: Joi.string(),
+  breed: Joi.string(),
+  description: Joi.string(),
+});
 
-const AddPetForm = ({ onSubmit }: AddPetFormProps) => {
-  const { register, handleSubmit } = useForm();
+export const AddPetForm = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
-  const submitForm = (data: any) => {
-    onSubmit(data);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm({
+    resolver: joiResolver(schema),
+    defaultValues: {
+      name: "daw",
+      breed: "awd",
+      description: "dw",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: {
+    name: string;
+    breed: string;
+    description: string;
+  }) => {
+    console.log(values);
+
+    try {
+      await axios.post("/api/servers", values);
+      form.reset();
+      router.refresh();
+      //window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  if (!isMounted) {
+    return null;
+  }
   return (
-    <form onSubmit={handleSubmit(submitForm)}>
-      <input {...register("name")} />
-      <input {...register("breed")} />
-      <textarea {...register("description")} />
-      <button type="submit">Add Pet</button>
-    </form>
+    <>
+      <div
+        className="backdrop-blur-md bg-white/10 border-4 transition ease-in-out hover:-translate-y-0 hover:scale-95 duration-100"
+        onClick={() => {
+          const modal = document.getElementById(
+            "my_modal_1"
+          ) as HTMLDialogElement;
+          if (modal !== null) {
+            modal.showModal();
+          }
+        }}
+      >
+        <h1 className="text-4xl font-bold text-white m-3">Add your pet</h1>
+        <p className="text-white m-3">
+          Review your pets and share your experience with others.
+        </p>
+      </div>
+
+
+
+      <dialog id="my_modal_1" className="modal ">
+     
+          <div className="modal-box bg-white ">
+            <h3 className="font-bold text-lg">
+              Hello! Enter your Pets information below:
+            </h3>
+            <p className="py-4">
+              Press ESC key or click the button below to close
+            </p>
+
+            <div className="gap-3 flex flex-col">
+              <Controller
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <input
+                    {...field}
+                    type="text"
+                    placeholder="Name"
+                    className="input input-bordered input-error w-full max-w-xs bg-red-100"
+                  />
+                )}
+              />
+
+              <Controller
+                control={form.control}
+                name="breed"
+                render={({ field }) => (
+                  <input
+                    {...field}
+                    type="text"
+                    placeholder="Breed"
+                    className="input input-bordered input-error w-full max-w-xs bg-red-100"
+                  />
+                )}
+              />
+
+              <Controller
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <input
+                    {...field}
+                    type="text"
+                    placeholder="Description"
+                    className="input input-bordered input-error w-full max-w-xs bg-white"
+                  />
+                )}
+              />
+            </div>
+            <div className="flex py-2 modal-action">
+              <form
+                method="dialog"
+                className="flex gap-2"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn bg-lime-400 text-white">Save</button>
+                <button
+                  className="btn bg-red-400 text-white"
+                  type="button" // Make sure it's not a submit button
+                  onClick={() => {
+                    const modal = document.getElementById(
+                      "my_modal_1"
+                    ) as HTMLDialogElement;
+                    if (modal !== null) {
+                      modal.close(); // Close the modal when the "Close" button is clicked
+                    }
+                  }}
+                >
+                  Close
+                </button>
+              </form>
+            </div>
+          </div>
+     
+      </dialog>
+    </>
   );
 };
-
-export default AddPetForm;
